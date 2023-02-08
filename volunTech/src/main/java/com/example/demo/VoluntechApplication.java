31d9d0aac849cb5bbf9783ca437d700d412bcd1b
Nadia <nadia_dj@hotmail.co.uk>
2023-02-08 09:53:52 +0000

    new image

---
+++ volunTech/src/main/java/com/example/demo/VoluntechApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntechApplication.class, args);
+
+		System.out.println("hello world!");
+	}
+
+
+}
