0a547d6abfe29c7da986bfa9d3086a8bf80464b8
Radhika <radhikarathi308@gmail.com>
2023-02-08 10:59:28 +0000

    adds charity repository

---
+++ volunTech/src/main/java/com/example/voluntech_jobboard/repositories/CharityRepository.java
@@ -0,0 +1,7 @@
+package com.example.voluntech_jobboard.repositories;
+
+import com.example.voluntech_jobboard.models.Charity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharityRepository extends JpaRepository<Charity, Long> {
+}
